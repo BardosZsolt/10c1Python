05bc382be1f9e8b7c2c2f3ad2240947396b03eea
Bárdos Zsolt Krisztián <bardos.zsolt@sulinet.csmszki>
2021-10-20 11:33:55 +0200

    20211020

---
+++ 20211020/Véletlenek.py
@@ -0,0 +1,38 @@
+from random import
+
+def feladat45():
+    a=randint(18,69)
+    print("a=",a)
+    b=randint(18,69)
+    print("b=",b)
+    nagyobb=max(a,b)
+    kisebb=min(a,b)
+    print("Hányaduk:" ,nagyobb/kisebb)
+    print("Hányadosuk:",nagyobb//kisebb)
+
+def feladat47():
+    szorzat=1
+    cv=1
+    while cv<=10:
+        gen=randint()
+        if gen%2==0
+            szorzat*=gen
+            print(gen,"\t",szorzat)
+            cv+=1
+
+def feladat48():
+    gen=randint(10,50)
+    print(gen,end=" ")
+    while gen !=25:
+        gen=randint(10,50)
+        print(gen,end=" ")
+    print()
+
+
+
+feladat45()
+feladat47()
+feladat48()
+
+
+    
